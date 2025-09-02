1d68e93c7964ece5d1ffdec0a85e97f11e4f223c
sonagara bhavin <sonagarabhavin23@gmail.com>
2025-09-02 14:00:31 +0530

    Update api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // backend ka base url
+  baseURL: "https://studenttrack-crud-authentication.onrender.com/api", // backend ka base url
   withCredentials: true, // cookies handle karne ke liye
 });
 
